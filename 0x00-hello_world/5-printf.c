1cbfe27399a39cff48592e121834b9085ce0f327
Sandra Dappa <sandrastevdappa@gmail.com>
2022-09-08 15:04:28 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - print the string in the printf
  *
- * Description: Using the main funtion 
+ * Description: Using the main funtion
  * this program prints "with proper grammar the outcome is a piece of art"
  * Return: 0
  */
