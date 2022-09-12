7eb4ad495dda19bd05badbef18aa5fab665634ee
Sandra Dappa <sandrastevdappa@gmail.com>
2022-09-12 08:09:56 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9
+ *
+ * Description: To Prints numbers between 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int 1;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
